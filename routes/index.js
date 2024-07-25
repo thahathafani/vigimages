dc90892854b481b62979a22906078a0c741d99fd
thahathafani <thahafazzin@gmail.com>
2024-07-25 13:34:23 +0530

    user and post imported

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const userModel = require("./users");
+const postModel = require("./post");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
